5091135ead1cacac866165c8cb52bd1843b53bfe
nithi <nithisha.1907@gmail.com>
2021-01-20 09:47:46 -0500

    Third Commit

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0,btnAdd,btnSub,btnMul,btnDiv,btnEqual,btnDot;
     Button btnClear;
     EditText ed1;
+    //Test Commit
     float Res1,Res2;
     boolean Add,Sub,Mul,Div;
     @Override
